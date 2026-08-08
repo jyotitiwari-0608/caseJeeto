644a89877933b23bd184cf0431bbe443321f55a6
Jyoti Tiwari <jt933558@gmail.com>
2026-08-08 18:21:59 +0530

    feat: log MongoDB connection on server startup

--- server/server.js
+++ server/server.js
@@ -99,6 +99,7 @@ const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI, { autoIndex: process.env.NODE_ENV !== 'production' })
   .then(() => {
+    console.log('connected to MongoDB');
     server.listen(PORT, () => console.log(`CaseLeeto server running on port ${PORT}`));
   })
   .catch((err) => {
